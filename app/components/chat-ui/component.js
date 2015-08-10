7fe2a1ca1ac23f464cd8e8ce82e750d712f072a5
Alex LaFroscia <alex@lafroscia.com>
2015-08-10 18:34:54 -0400

    Add header to chat UI

--- app/components/chat-ui/component.js
+++ app/components/chat-ui/component.js
@@ -5,12 +5,14 @@ const { alias } = computed;
 
 export default Component.extend({
 
-  classNames: ['grid-block', 'medium-10'],
+  classNames: ['medium-10'],
 
   isGlimmerComponent: true,
 
   conversation: alias('model'),
 
-  sender: alias('conversation.sender')
+  sender: alias('conversation.sender'),
+
+  messages: alias('conversation.messages')
 
 });
